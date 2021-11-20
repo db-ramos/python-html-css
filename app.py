0a63fd43b9b7a8d0e4b8c15287643ed8ac34be37
David Ramos <davidness@gmail.com>
2021-11-20 06:58:11 +0000

    Projeto Blog em Flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, request, redirect, url_for, flash, session
 
 DATABASE = "blog.db"
 SECRET_KEY = "pudim"
@@ -25,4 +25,34 @@ def exibir_entradas():
     entradas = []
     for titulo, texto in cur.fetchall():
         entradas.append({'titulo': titulo, 'texto': texto})
-    return str(entradas)
+    return render_template("exibir_entradas.html", entradas=entradas)
+
+#Esta rota receberá os dados do formulário de "templates/exibir_entradas.html"
+# methods=["POST"] -> configura a rota para receber requisições de formulários HTML com método POST (<form method=POST>)
+@app.route('/inserir', methods=["POST"])
+def inserir_entrada():
+    titulo = request.form['titulo']
+    texto = request.form['texto']
+    sql = "insert INTO entradas(titulo,texto) values (?,?)"
+    g.bd.execute(sql,[titulo,texto])
+    g.bd.commit()
+    flash("Postagem registrada. Valeu!!")
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form["username"] != "admin" or request.form['password'] != "admin":
+            erro = "Usuário ou senha inválidos"
+        else:
+            session['logado'] = True
+            flash("Login efetuado com sucesso")
+            return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
+
+@app.route("/logout")
+def logout():
+    session.pop("logado",None)
+    flash("Volte sempre!!")
+    return redirect(url_for("exibir_entradas"))
